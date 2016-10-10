f6fa6f191b1d575d87f894f35071b63b153bd185
Sotiris Ioannou <me@sotiris.engineer>
2016-10-10 19:57:08 -0400

    Added a circle on stage.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,36 @@
  * Created by sotiris on 10/9/16.
  */
 'use strict';
+(function () {
 
-console.log(PIXI);
+    // variables
+    var circle;
 
-console.log('kokos');
+
+    console.log(PIXI);
+
+    var renderer = PIXI.autoDetectRenderer(500,500);
+
+    document.body.appendChild(renderer.view);
+
+    var stage = new PIXI.Container();
+
+
+
+
+    //create circle
+    circle = new PIXI.Graphics();
+    circle.beginFill("0x66CCFF");
+    circle.drawCircle(100,100,32);
+    circle.endFill();
+
+    stage.addChild(circle);
+
+    
+    renderer.render(stage);
+
+
+}());
+
+
+
